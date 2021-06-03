0a53444f265ba5801f6263d8d436790dd2b1c6a4
FloatingDream528 <82099714+FloatingDream528@users.noreply.github.com>
2021-06-03 21:41:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,3 +20,4 @@ git clone https://github.com/lisaac/luci-lib-docker.git package/lean/luci-lib-do
 svn co https://github.com/openwrt/luci/trunk/applications/luci-app-sqm
 #自定义设置
 sed -i '32d' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/hd0/hd1/g' package/boot/grub2/files/grub-early.cfg
