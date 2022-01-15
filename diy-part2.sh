4c5aaf2b9061c9672192da22d88409515a2ae468
FloatingDream528 <82099714+FloatingDream528@users.noreply.github.com>
2022-01-15 21:41:11 +0800

    Add files via upload

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,5 +16,17 @@
 #添加额外软件包
 git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 git clone https://github.com/fw876/helloworld.git package/helloworld
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/
+#删除passwall重复组件
+rm -rf package/passwall/naiveproxy
+rm -rf package/passwall/shadowsocks-rust
+rm -rf package/passwall/shadowsocksr-libev
+rm -rf package/passwall/simple-obfs
+rm -rf package/passwall/tcping
+rm -rf package/passwall/trojan
+rm -rf package/passwall/v2ray-core
+rm -rf package/passwall/v2ray-geodata
+rm -rf package/passwall/v2ray-plugin
+rm -rf package/passwall/xray-core
+rm -rf package/passwall/xray-plugin
